51376161a9ea34f12c21ee2952f60e2437f1b4d2
monirjhossain <md.monirjhossain@gmail.com>
2020-10-08 18:44:05 +0600

    08/10/2020

---
+++ project2/wp-content/themes/zBoomMusic/header.php
@@ -0,0 +1,24 @@
+<nav>
+	<div class="wrap-nav zerogrid">
+		<div class="menu">
+			<ul>
+				<li class="current"><a href="index.html">Home</a></li>
+				<li><a href="blog.html">Blog</a></li>
+				<li><a href="gallery.html">Gallery</a></li>
+				<li><a href="single.html">About</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</div>
+		
+		<div class="minimenu"><div>MENU</div>
+			<select onchange="location=this.value">
+				<option></option>
+				<option value="index.html">Home</option>
+				<option value="blog.html">Blog</option>
+				<option value="gallery.html">Gallery</option>
+				<option value="single.html">About</option>
+				<option value="contact.html">Contact</option>
+			</select>
+		</div>		
+	</div>
+</nav>
